936ef28d0a1f2ba8887aec03b67076c7c6aa988d
Bruno Razera <brunocrazera@gmail.com>
2016-03-14 22:53:22 -0300

    Entrega 4 - Herança

---
+++ manada.js
@@ -0,0 +1,78 @@
+function Animal() {
+  this.barulho = "barulho"
+};
+
+Animal.prototype = {
+  fazerBarulho: function () {
+    return this.barulho;
+  }
+};
+
+function Cao() {
+  this.barulho = "Au";
+};
+
+Cao.prototype = new Animal();
+
+function Gato() {
+  this.barulho = "Miau";
+};
+
+Gato.prototype = new Animal();
+
+function Manada() {
+  this.animais = [];
+};
+
+Manada.prototype = {
+  adicionar: function (animal) {
+    this.animais.push(animal);
+    return console.log("Animal adicionado");
+  }
+};
+
+function ManadaVirgula() {
+  Manada.call(this);
+};
+
+ManadaVirgula.prototype = new Manada();
+ManadaVirgula.prototype.barulhos = function () {
+  barulho = [];
+  this.animais.forEach(function (animal) {
+    barulho.push(animal.fazerBarulho());
+  });
+  return barulho.join(", ");
+};
+
+function ManadaSustenidaDupla() {
+  Manada.call(this);
+}
+
+ManadaSustenidaDupla.prototype = new Manada();
+ManadaSustenidaDupla.prototype.barulhos = function () {
+  barulho = [];
+  this.animais.forEach(function (animal) {
+    barulho.push(animal.fazerBarulho());
+  });
+  return barulho.concat(barulho).sort().join("# ");
+}
+
+
+
+var manada = new Manada();
+var manadaVirgula = new ManadaVirgula();
+var manadaSustenidaDupla = new ManadaSustenidaDupla();
+var animais = [new Cao(), new Gato()];
+
+animais.forEach(function (animal) {
+  manadaVirgula.adicionar(animal);
+manadaSustenidaDupla.adicionar(animal);
+});
+
+
+console.log(manadaVirgula.lista_animais);
+// Print Esperado: Au, Miau
+console.log(manadaVirgula.barulhos());
+
+// Print Esperado: Au# Au# Miau# Miau
+console.log(manadaSustenidaDupla.barulhos());
